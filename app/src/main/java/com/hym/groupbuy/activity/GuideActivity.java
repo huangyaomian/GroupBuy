8d9e71506481fddaa315a397c4dd27f0431818a2
mika <345249887@qq.com>
2020-05-15 09:03:40 +0800

    1

--- app/src/main/java/com/hym/groupbuy/activity/GuideActivity.java
+++ app/src/main/java/com/hym/groupbuy/activity/GuideActivity.java
@@ -1,37 +1,117 @@
 package com.hym.groupbuy.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.viewpager.widget.ViewPager;
+
+import com.hym.groupbuy.MainActivity;
 import com.hym.groupbuy.R;
+import com.hym.groupbuy.adapter.MyGuideAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GuideActivity extends BaseActivity {
+
+    @BindView(R.id.guide_vp)
+    ViewPager guideVp;
+    @BindView(R.id.guide_btn)
+    Button guideBtn;
+
+    private List<View> mViewList;
+
+    int[] imgRes = new int[]{
+            R.mipmap.guide_1,
+            R.mipmap.guide_2,
+            R.mipmap.guide_3,
+            R.mipmap.guide_4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+        init();
+        initView();
+        initData();
+        initEvent();
+
+
     }
 
     @Override
     public void init() {
+        mViewList = new ArrayList<>();
 
     }
 
     @Override
     public void initView() {
-
+        MyGuideAdapter adapter = new MyGuideAdapter(mViewList);
+        guideVp.setAdapter(adapter);
     }
 
     @Override
     public void initData() {
-
+        for (int i = 0; i < imgRes.length; i++) {
+            View inflate = getLayoutInflater().inflate(R.layout.guide_item, null);
+            ImageView ivGuide = inflate.findViewById(R.id.guide_item_iv);
+            ivGuide.setBackgroundResource(imgRes[i]);
+            mViewList.add(inflate);
+        }
     }
 
     @Override
     public void initEvent() {
+        guideVp.setOnPageChangeListener(new Mylistner());
+    }
 
+    @OnClick({R.id.guide_vp, R.id.guide_btn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.guide_vp:
+                break;
+            case R.id.guide_btn:
+                startActivity(new Intent(GuideActivity.this, MainActivity.class));
+                finish();
+                break;
+        }
+    }
+
+    class Mylistner implements ViewPager.OnPageChangeListener{
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            //假如ViewPager滚动到最后一张就显示button
+            if (position == mViewList.size()-1){
+                guideBtn.setVisibility(View.VISIBLE);
+                Animation animation = AnimationUtils.loadAnimation(GuideActivity.this, R.anim.anim_guide_btn_start);
+                guideBtn.startAnimation(animation);
+
+            }else{
+                guideBtn.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/hym/groupbuy/adapter/MyGuideAdapter.java
@@ -0,0 +1,47 @@
+package com.hym.groupbuy.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 引导界面viewpager适配器
+ */
+
+public class MyGuideAdapter extends PagerAdapter {
+
+    private List<View> mViewList = new ArrayList<>();
+
+    public MyGuideAdapter(List<View> mViewList){
+        super();
+        this.mViewList = mViewList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = mViewList.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(mViewList.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mViewList == null?0:mViewList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;//官方推荐的写法
+    }
+}
